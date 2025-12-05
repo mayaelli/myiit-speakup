47e697dad8847099eb3bea7f068c4f1d48dd73d6
maya. <alimarhamah9@gmail.com>
2025-12-05 19:55:47 +0800

    firebase config updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqRfeSLKkuykc-vNkKLbvQHUIpZ9IaSU0",
-  authDomain: "bb-speakup.firebaseapp.com",
-  projectId: "bb-speakup",
-  storageBucket: "bb-speakup.firebasestorage.app",
-  messagingSenderId: "609466283176",
-  appId: "1:609466283176:web:2069d8971593fd0ab03e9d",
-  measurementId: "G-6L0J9Z5PS0"
+  apiKey: "AIzaSyAXdM6Fh-e7Gy68JS1JkT9L7ulX-HfaMZE",
+  authDomain: "myiitspeakup.firebaseapp.com",
+  projectId: "myiitspeakup",
+  storageBucket: "myiitspeakup.firebasestorage.app",
+  messagingSenderId: "35615024224",
+  appId: "1:35615024224:web:7c915c724262fff887cf05",
+  measurementId: "G-2XNKXLH7C7"
 };
 
 const app = initializeApp(firebaseConfig);
